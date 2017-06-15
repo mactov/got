07acd360281e6b7f3f0fabb97ca39fc14660f4c1
mactov <mactov@iablaka.com>
2017-06-15 23:52:25 +0200

    Language, ConceptualWord and TranslatedWord classes created

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+
+class ConceptualWord(models.Model):
+    formal = models.BooleanField()
+    description = models.CharField(max_length=255)
+
+class TranslatedWord(models.Model):
+    conceptual_word = models.ForeignKey(ConceptualWord)
+    language = models.ForeignKey(Language)
+    translation = models.CharField(max_length=100)
